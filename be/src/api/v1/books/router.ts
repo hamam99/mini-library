caf62700693909b08031dd047701d600c7a3e59e
hamam <hamam.nasrodin@appfuxion.com>
2025-05-20 17:33:56 +0800

    add upload file

--- be/src/api/v1/books/router.ts
+++ be/src/api/v1/books/router.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createBook, deleteBook, getAllBooks, getBookById, updateBook } from './controller';
+import multerMiddleware from '../../../middlewares/multer';
 
 const booksRouter = express();
 
 booksRouter.get('/books', getAllBooks);
 booksRouter.get('/books/:id', getBookById);
-booksRouter.post('/books', createBook);
+booksRouter.post('/books', multerMiddleware.single('file'), createBook);
 booksRouter.delete('/books/:id', deleteBook);
 booksRouter.put('/books/:id', updateBook);
 
