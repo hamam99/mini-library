de210253af939670a6fb425ae7d1335ce5d16d4c
hamam <hamam.nasrodin@appfuxion.com>
2025-05-20 17:55:24 +0800

    add config

--- be/src/config/config.ts
+++ be/src/config/config.ts
@@ -6,12 +6,14 @@ interface Config {
   port: number;
   urlMongoDb: string;
   nodeEnv?: string;
+  baseUrl?: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   urlMongoDb: process.env.URL_MONGODB_DEV || '',
   nodeEnv: process.env.NODE_ENV || 'development',
+  baseUrl: `${process.env.BASE_URL}:${process.env.PORT || 3000}`,
 };
 
 export default config;
--- be/src/middlewares/multer.ts
+++ be/src/middlewares/multer.ts
@@ -1,8 +1,9 @@
 import multer, { StorageEngine } from 'multer';
 
+export const PATH_UPLOAD_BOOKS = 'public/books/';
 const storage: StorageEngine = multer.diskStorage({
   destination: function (req: Express.Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
-    cb(null, 'public/uploads/');
+    cb(null, PATH_UPLOAD_BOOKS);
   },
   filename: function (req: Express.Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
     cb(null, Date.now() + '-' + file.originalname);
