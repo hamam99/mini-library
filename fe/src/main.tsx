a88b19b136bb6fe83d2a2a52aace86768db967bf
hamam <hamam.nasrodin@appfuxion.com>
2025-06-02 13:35:48 +0800

    add navigation

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Home from './pages/Home/index.tsx';
+import ReadBook from './pages/ReadBook/index.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "/read-book",
+    Component: ReadBook
+  },
+]);
+
+const root = document.getElementById('root')!
+createRoot(root).render(
+  <StrictMode><RouterProvider router={router} /></StrictMode>
+);
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
---
+++ fe/src/pages/ReadBook/index.tsx
@@ -0,0 +1,9 @@
+const ReadBook = () => {
+  return <div>
+    <h1>
+      READ BOOK
+    </h1>
+  </div>
+}
+
+export default ReadBook
